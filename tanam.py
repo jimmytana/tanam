de10f241ac9c23255dd777962e2d5c8094ae291a
Jimmy <kubwimanajimmy96@gmail.com>
2025-03-17 18:22:27 +0200

    version 4

--- tanam.py
+++ tanam.py
@@ -10,9 +10,33 @@
 # print(len(txt.replace(" ", "")))
 
 
-word = input(f"Enter five phrases separated by |: ")
-word = word.split("|")
-for x in word:
-    print(x,":",len(x.replace(" ", "")))
+# word = input(f"Enter five phrases separated by |: ")
+# word = word.split("|")
+# for x in word:
+#     print(x,":",len(x.replace(" ", "")))
 
+def encrypt(salt, plaintxt):
+    if(salt.find(plaintxt) >= 0):
+        if(salt.find(plaintxt) % 2 == 0):
+            ind = salt.find(plaintxt)
+            replacement = salt[(ind+1)]
+            return replacement
+        else:
+            ind = salt.find(plaintxt)
+            replacement = salt[(ind-1)]
+            return replacement
+    else:
+        return plaintxt
+# def decrypt(cipher):
 
+salt = "kajipotefu"
+txt = input("Enter a letter: ")
+txt = txt.lower()
+txt = list(txt)
+result = ""
+for i in range(len(txt)):
+    txt[i] = encrypt(salt, str(txt[i]))
+
+for i in txt:
+    result += i
+print("The encrypted text: ", result)
