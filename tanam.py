980b912a77ecefe4c608eb15d803e5ec157a9eb7
Jimmy <kubwimanajimmy96@gmail.com>
2025-03-17 18:21:49 +0200

    version 3

--- tanam.py
+++ tanam.py
@@ -4,7 +4,15 @@
 # print(len(txt))
 
 
-"""version 2"""
-print("please give us your text: ", end="")
-txt = input()
-print(len(txt.replace(" ", "")))
+# """version 2"""
+# print("please give us your text: ", end="")
+# txt = input()
+# print(len(txt.replace(" ", "")))
+
+
+word = input(f"Enter five phrases separated by |: ")
+word = word.split("|")
+for x in word:
+    print(x,":",len(x.replace(" ", "")))
+
+
